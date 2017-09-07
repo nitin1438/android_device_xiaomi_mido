1d1576dfa03d83db7c5a6f3ef2d62e427c1c7f4b
sathish2026 <txt2sathish@gmail.com>
2017-09-07 23:46:24 +0530

    add ims shims

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -21,13 +21,18 @@ LOCAL_SRC_FILES := get_process_name.c
 LOCAL_MODULE := libshims_get_process_name
 LOCAL_MODULE_TAGS := optional
 
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
 LOCAL_SRC_FILES := rild_socket.c
 
 LOCAL_MODULE := rild_socket
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := MediaBuffer.cpp
+
+LOCAL_SHARED_LIBRARIES          += libui libgui libstagefright_foundation
+LOCAL_C_INCLUDES                += framework/native/include frameworks/av/include
+LOCAL_CFLAGS                    += -Wno-unused-private-field
+
+LOCAL_MODULE := libshims_ims
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
