AospExtended-Devices/device_xiaomi_mido

mido: Update init for O
Change-Id: Ic43e817ace142b0380525de63a45f9ac714c8ed3

init/init_mido.cpp
@@ -29,17 +29,19 @@
 #include <stdlib.h>
 #include <sys/sysinfo.h>
 
-#include "vendor_init.h"
+#include <android-base/properties.h>
 #include "property_service.h"
-#include "log.h"
-#include "util.h"
+#include "vendor_init.h"
 
-char const *heapstartsize;
-char const *heapgrowthlimit;
-char const *heapsize;
-char const *heapminfree;
-char const *heapmaxfree;
-char const *large_cache_height;
+using android::base::GetProperty;
+using android::base::SetProperty;
+
+std::string heapstartsize;
+std::string heapgrowthlimit;
+std::string heapsize;
+std::string heapminfree;
+std::string heapmaxfree;
+std::string large_cache_height;
 
 static void init_alarm_boot_properties()
 {
@@ -65,11 +67,7 @@ static void init_alarm_boot_properties()
      * 7 -> CBLPWR_N pin toggled (for external power supply)
      * 8 -> KPDPWR_N pin toggled (power key pressed)
      */
-     if (boot_reason == 3) {
-        property_set("ro.alarm_boot", "true");
-     } else {
-        property_set("ro.alarm_boot", "false");
-     }
+     SetProperty("ro.alarm_boot", boot_reason == 3 ? "true" : "false");
 }
 
 void check_device()
@@ -85,14 +83,14 @@ void check_device()
         heapsize = "512m";
         heapminfree = "4m";
         heapmaxfree = "8m";
-	large_cache_height = "2048";
+        large_cache_height = "2048";
     } else if (sys.totalram > 2048ull * 1024 * 1024) {
         // from - phone-xxhdpi-3072-dalvik-heap.mk
         heapstartsize = "8m";
         heapgrowthlimit = "288m";
         heapsize = "768m";
         heapminfree = "512k";
-	heapmaxfree = "8m";
+        heapmaxfree = "8m";
         large_cache_height = "1024";
     } else {
         // from - phone-xxhdpi-2048-dalvik-heap.mk
@@ -110,22 +108,22 @@ void vendor_load_properties()
     init_alarm_boot_properties();
     check_device();
 
-    property_set("dalvik.vm.heapstartsize", heapstartsize);
-    property_set("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
-    property_set("dalvik.vm.heapsize", heapsize);
-    property_set("dalvik.vm.heaptargetutilization", "0.75");
-    property_set("dalvik.vm.heapminfree", heapminfree);
-    property_set("dalvik.vm.heapmaxfree", heapmaxfree);
+    SetProperty("dalvik.vm.heapstartsize", heapstartsize);
+    SetProperty("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+    SetProperty("dalvik.vm.heapsize", heapsize);
+    SetProperty("dalvik.vm.heaptargetutilization", "0.75");
+    SetProperty("dalvik.vm.heapminfree", heapminfree);
+    SetProperty("dalvik.vm.heapmaxfree", heapmaxfree);
 
-    property_set("ro.hwui.texture_cache_size", "72");
-    property_set("ro.hwui.layer_cache_size", "48");
-    property_set("ro.hwui.r_buffer_cache_size", "8");
-    property_set("ro.hwui.path_cache_size", "32");
-    property_set("ro.hwui.gradient_cache_size", "1");
-    property_set("ro.hwui.drop_shadow_cache_size", "6");
-    property_set("o.hwui.texture_cache_flushrate", "0.4");
-    property_set("ro.hwui.text_small_cache_width", "1024");
-    property_set("ro.hwui.text_small_cache_height", "1024");
-    property_set("ro.hwui.text_large_cache_width", "2048");
-    property_set("ro.hwui.text_large_cache_height", large_cache_height);
+    SetProperty("ro.hwui.texture_cache_size", "72");
+    SetProperty("ro.hwui.layer_cache_size", "48");
+    SetProperty("ro.hwui.r_buffer_cache_size", "8");
+    SetProperty("ro.hwui.path_cache_size", "32");
+    SetProperty("ro.hwui.gradient_cache_size", "1");
+    SetProperty("ro.hwui.drop_shadow_cache_size", "6");
+    SetProperty("o.hwui.texture_cache_flushrate", "0.4");
+    SetProperty("ro.hwui.text_small_cache_width", "1024");
+    SetProperty("ro.hwui.text_small_cache_height", "1024");
+    SetProperty("ro.hwui.text_large_cache_width", "2048");
+    SetProperty("ro.hwui.text_large_cache_height", large_cache_height);
 }

nitin1438/android_device_xiaomi_mido

init/init_mido.cpp
@@ -29,17 +29,19 @@
 #include <stdlib.h>
 #include <sys/sysinfo.h>
 
-#include "vendor_init.h"
+#include <android-base/properties.h>
 #include "property_service.h"
-#include "log.h"
-#include "util.h"
+#include "vendor_init.h"
 
-char const *heapstartsize;
-char const *heapgrowthlimit;
-char const *heapsize;
-char const *heapminfree;
-char const *heapmaxfree;
-char const *large_cache_height;
+using android::base::GetProperty;
+using android::base::SetProperty;
+
+std::string heapstartsize;
+std::string heapgrowthlimit;
+std::string heapsize;
+std::string heapminfree;
+std::string heapmaxfree;
+std::string large_cache_height;
 
 static void init_alarm_boot_properties()
 {
@@ -65,11 +67,7 @@ static void init_alarm_boot_properties()
      * 7 -> CBLPWR_N pin toggled (for external power supply)
      * 8 -> KPDPWR_N pin toggled (power key pressed)
      */
-     if (boot_reason == 3) {
-        property_set("ro.alarm_boot", "true");
-     } else {
-        property_set("ro.alarm_boot", "false");
-     }
+     SetProperty("ro.alarm_boot", boot_reason == 3 ? "true" : "false");
 }
 
 void check_device()
@@ -85,14 +83,14 @@ void check_device()
         heapsize = "512m";
         heapminfree = "4m";
         heapmaxfree = "8m";
-	large_cache_height = "2048";
+        large_cache_height = "2048";
     } else if (sys.totalram > 2048ull * 1024 * 1024) {
         // from - phone-xxhdpi-3072-dalvik-heap.mk
         heapstartsize = "8m";
         heapgrowthlimit = "288m";
         heapsize = "768m";
         heapminfree = "512k";
-	heapmaxfree = "8m";
+        heapmaxfree = "8m";
         large_cache_height = "1024";
     } else {
         // from - phone-xxhdpi-2048-dalvik-heap.mk
@@ -110,22 +108,21 @@ void vendor_load_properties()
     init_alarm_boot_properties();
     check_device();
 
-    property_set("dalvik.vm.heapstartsize", heapstartsize);
-    property_set("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
-    property_set("dalvik.vm.heapsize", heapsize);
-    property_set("dalvik.vm.heaptargetutilization", "0.75");
-    property_set("dalvik.vm.heapminfree", heapminfree);
-    property_set("dalvik.vm.heapmaxfree", heapmaxfree);
-
-    property_set("ro.hwui.texture_cache_size", "72");
-    property_set("ro.hwui.layer_cache_size", "48");
-    property_set("ro.hwui.r_buffer_cache_size", "8");
-    property_set("ro.hwui.path_cache_size", "32");
-    property_set("ro.hwui.gradient_cache_size", "1");
-    property_set("ro.hwui.drop_shadow_cache_size", "6");
-    property_set("ro.hwui.texture_cache_flushrate", "0.4");
-    property_set("ro.hwui.text_small_cache_width", "1024");
-    property_set("ro.hwui.text_small_cache_height", "1024");
-    property_set("ro.hwui.text_large_cache_width", "2048");
-    property_set("ro.hwui.text_large_cache_height", large_cache_height);
+    SetProperty("dalvik.vm.heapstartsize", heapstartsize);
+    SetProperty("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+    SetProperty("dalvik.vm.heapsize", heapsize);
+    SetProperty("dalvik.vm.heaptargetutilization", "0.75");
+    SetProperty("dalvik.vm.heapminfree", heapminfree);
+    SetProperty("dalvik.vm.heapmaxfree", heapmaxfree);
+    SetProperty("ro.hwui.texture_cache_size", "72");
+    SetProperty("ro.hwui.layer_cache_size", "48");
+    SetProperty("ro.hwui.r_buffer_cache_size", "8");
+    SetProperty("ro.hwui.path_cache_size", "32");
+    SetProperty("ro.hwui.gradient_cache_size", "1");
+    SetProperty("ro.hwui.drop_shadow_cache_size", "6");
+    SetProperty("ro.hwui.texture_cache_flushrate", "0.4");
+    SetProperty("ro.hwui.text_small_cache_width", "1024");
+    SetProperty("ro.hwui.text_small_cache_height", "1024");
+    SetProperty("ro.hwui.text_large_cache_width", "2048");
+    SetProperty("ro.hwui.text_large_cache_height", large_cache_height);
 }